2cbf09b9c7391750dc651e74e38a685fc231198b
Joel Bentley <joel-bentley@users.noreply.github.com>
2016-08-16 19:34:17 +0000

    completed project

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var url = require('url');
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var port = 8080;
+
+var server = http.createServer(function(req, res) {
+    var parsedUrl = url.parse(req.url, true);
+
+    var urlSpace = /%20/g;
+    var forwardSlash = /\//g;
+    var extraCharacters = /[^a-zA-z\s\d]/g;
+    
+    var parsedInput = parsedUrl.pathname
+            .slice(1)
+            .replace(urlSpace, ' ')
+            .replace(forwardSlash, ' ')
+            .replace(extraCharacters, '');
+    
+    var result = parseDate(parsedInput);
+    
+    if (result) {
+       res.writeHead(200, { 'Content-Type': 'application/json' });
+       res.end(JSON.stringify(result));
+    } else {
+       res.writeHead(404);
+       res.end();
+    }
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+server.listen(port, function() {
+    console.log('Listening on port ' + port);
 });
+
+
+function parseDate(input) {
+
+    var momentDate = moment(input, ['MMMM D YYYY', 'D MMMM YYYY'], true);
+    
+    if (!momentDate.isValid()) {
+        momentDate = moment(input, 'X', true);
+    }
+    
+    if (!momentDate.isValid()) {
+        return {
+            unix: null,
+            natural: null
+        };
+    }
+    
+    return {
+        unix: momentDate.format('X'),
+        natural: momentDate.format('MMMM D, YYYY')
+    };
+}
