4827b76b6aadcd26587d533e091218bb5208a059
Joel Bentley <joel-bentley@users.noreply.github.com>
2016-08-16 20:17:02 +0000

    edit port for Heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 var moment = require('moment');
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var server = http.createServer(function(req, res) {
     var parsedUrl = url.parse(req.url, true);
